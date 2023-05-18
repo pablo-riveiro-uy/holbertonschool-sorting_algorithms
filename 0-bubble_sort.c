804fd65d00dee7b2aef5d0939bca85e0799d0c4b
TheWhiteMachine <priveiro@gmail.com>
2023-05-17 17:04:38 -0700

    fixing output

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,17 +1,23 @@
 #include "sort.h"
-
+/**
+ * bubbble_sort - bubble sort algorith to sort an array
+ * @array: an array of ints
+ * @size: size of array
+ */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, tempNum;
+
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		for (j = 0; j < (size - i) - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				tempNum = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = tempNum;
+				print_array(array, size);
 			}
 		}
 	}
