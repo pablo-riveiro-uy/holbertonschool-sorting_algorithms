c12ede5b57c1a7e878ffe5203089339b433f0787
fabianatellechea <fabianatellechea@gmail.com>
2023-05-18 16:13:03 -0700

    task 1 working

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+/**
+ *insertion_sort_list-sorts doubly linked list of int in ascending
+ *order using Insertion sort algorithm
+ *@list: list
+ *Return: void
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current = *list, *tmp, *aux;
+
+	if (!(*list))
+	{
+		return;
+	}
+
+	while ((current = current->next))
+	{
+		tmp = current;
+
+		while (tmp->prev && tmp->prev->n > tmp->n)
+		{
+			aux = tmp->prev;
+
+			if (tmp->next)
+			{
+				tmp->next->prev = aux;
+			}
+			if (aux->prev)
+			{
+				aux->prev->next = tmp;
+			}
+			else
+			{
+				*list = tmp;
+			}
+
+			aux->next = tmp->next;
+			tmp->prev = aux->prev;
+			tmp->next = aux;
+			aux->prev = tmp;
+			print_list(*list);
+		}
+	}
+}
