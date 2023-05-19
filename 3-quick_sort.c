0c6c83b8ea2201aeafda88d32fceecc18d1640ef
root <priveiro@gmail.com>
2023-05-19 09:24:51 -0700

    print on swap

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -9,43 +9,13 @@
 void quick_sort(int *array, size_t size)
 {
 
-/* 	if (array == NULL)
-		return;
-
-	if (1 == is_same_num(array))
-		return;
-
-	if (size < 2)
-		return; */
-	
 	quick_sort_recursion(array, 0, size - 1, size);
 }
 
-/**
- * is_same_num - detects if an array is a one number tepetition
- * @array: an array to divide and sort
- *
- * Return: 1 for true 0 for false
- */
 
-int is_same_num(int *array)
-{
-	int i = 0;
-	int is_same = 1;
-
-	while (array[i - 1])
-	{
-		if (array[i] == array[i + 1])
-			is_same = 1;
-		else
-			is_same = 0;
-	}
-	return (is_same);
-
-}
 
 /**
- *quick_sort_recursion - make a recursion to execute quick sort algorithm
+ * quick_sort_recursion - make a recursion to execute quick sort algorithm
  * @array: an array of ints
  * @low: the start of the new partition
  * @high: the end of the new partition
@@ -54,7 +24,6 @@ int is_same_num(int *array)
 
 void quick_sort_recursion(int array[], int low, int high, int size)
 {
-
 	if (low < high)
 	{
 	int pivot_index = partition(array, low, high, size);
@@ -64,17 +33,25 @@ void quick_sort_recursion(int array[], int low, int high, int size)
 	}
 }
 
+
 /**
  * swap - swap the value of to variables
  * @a: a value
  * @b: b value
+ * @array: array to print
+ * @size: size of array
 */
 
-void swap(int *a, int *b)
+void swap(int *a, int *b, int *array, size_t size)
 {
 	int temp = *a;
+
+	if (*a != *b)
+	{
 	*a = *b;
 	*b = temp;
+	print_array(array, size);
+	}
 }
 
 /**
@@ -87,7 +64,7 @@ void swap(int *a, int *b)
  * Return: index of pivot for recursion
  */
 
-int partition(int array[], int low, int high, int size)
+int partition(int array[], int low, int high, size_t size)
 {
 	int pivot_value = array[high];
 	int i = low - 1;
@@ -98,14 +75,9 @@ int partition(int array[], int low, int high, int size)
 		if  (array[j] < pivot_value)
 		{
 			i++;
-			swap(&array[i], &array[j]);
-			if (i != j)
-			{
-			print_array(array, size);
-			}
+			swap(&array[i], &array[j], array, size);
 		}
 	}
-	swap(&array[i + 1], &array[high]);
-	print_array(array, size);
+	swap(&array[i + 1], &array[high], array, size);
 	return (i + 1);
 }
--- sort.h
+++ sort.h
@@ -28,7 +28,8 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void quick_sort_recursion(int array[], int low, int high,  int size);
-int partition(int array[], int low, int high, int size);
+void swap(int *a, int *b, int *array, size_t size);
+int partition(int array[], int low, int high, size_t size);
 int is_same_num(int *array);
 
 
